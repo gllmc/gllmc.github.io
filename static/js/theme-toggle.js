8a4a2e14125e781dd15598d29ae7edfa689cbd21
Guillaume Chirache <22531188+gllmc@users.noreply.github.com>
2025-10-12 17:03:11 +0200

    Define theme color meta according to light/dark mode

--- static/js/theme-toggle.js
+++ static/js/theme-toggle.js
@@ -1,6 +1,7 @@
 const themeToggleButton = document.getElementById("theme-toggle");
 const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
 const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+const themeColorMetaTag = document.getElementById("theme-color-meta");
 
 if (
     localStorage.getItem("color-theme") === "dark" ||
@@ -16,6 +17,10 @@ themeToggleButton.addEventListener("click", function () {
     const isCurrentlyDark = document.documentElement.classList.contains("dark");
 
     document.documentElement.classList.toggle("dark", !isCurrentlyDark);
+    themeColorMetaTag.setAttribute(
+        "content",
+        isCurrentlyDark ? "#f9fafb" : "#111827",
+    );
     themeToggleLightIcon.classList.toggle("hidden", isCurrentlyDark);
     themeToggleDarkIcon.classList.toggle("hidden", !isCurrentlyDark);
     localStorage.setItem("color-theme", isCurrentlyDark ? "light" : "dark");
